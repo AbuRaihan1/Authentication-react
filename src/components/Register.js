2ca7ad106a40531952bc043ea52fe06df8b8f17b
Abu Raihan <raihanahmed01973@gmail.com>
2023-01-30 13:51:23 +0600

    done home page conditionally rendering

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/UserContext";
 
 const Register = () => {
@@ -126,9 +127,9 @@ const Register = () => {
         </div>
         <p className="px-6 text-sm text-center text-gray-400">
           Already have an account yet?{" "}
-          <a href="#" className="hover:underline text-gray-600">
+          <Link to="/login" className="hover:underline text-gray-600">
             Sign In
-          </a>
+          </Link>
           .
         </p>
       </div>
